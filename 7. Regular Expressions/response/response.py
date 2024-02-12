661d4cf3f0b779556509766846f519a6bb57ab0d
madefromjames <madefromjames@gmail.com>
2024-02-12 14:21:02 +0100

    Response Validation

---
+++ 7. Regular Expressions/response/response.py
@@ -0,0 +1,13 @@
+import validators
+
+def main():
+    email = input("Email: ")
+
+    if validators.email(email):
+        print("Valid")
+    else:
+        print("Invalid")
+
+
+if __name__ == "__main__":
+    main()
